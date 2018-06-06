ca62aeecaf2cdae474215f8e7a25b6649c71db81
kjazleen <kaurjasleen56583@gmail.com>
2018-06-07 02:49:08 +1000

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,36 @@
+#Q1
+print("I am Jasleen")#print anything
+
+
+
+#Q2
+print("Acad"+"view")#join two string
+
+
+
+#Q3
+x=1
+y=2
+z=3
+print(x,y,z )#print values of x,y,z
+
+
+
+#Q4
+print("Let\'s get started")#print Let's get started
+
+
+#Q5
+s="Acadview"
+course="python"
+fee=5000
+print("hi you are in %s,your course is %s and fee is %d"%(s,course,fee))
+
+
+#Q6
+name="Tommy Stark"
+salary=1000000
+print("%s,%d"%(name,salary))
+
+
+
