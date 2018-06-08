1a7f452b40efc72823f5a0d828501dae96f7c4b8
kjazleen <kaurjasleen56583@gmail.com>
2018-06-08 19:00:27 +1000

    assignment

---
+++ assignment4.py
@@ -0,0 +1,38 @@
+#Q1
+a=(1,2,3)
+b=len(a)
+print(b)
+
+#Q2
+a=(1,2,3,4)
+print(max(a))
+print(min(a))
+
+#Q3
+a=(1*2*3*4)
+print(a)
+
+#Q4
+a=set([1,4,2,3])
+print(a)
+
+
+#Q5
+d={'name':'jasleen',
+   'course':'python'}
+print(d)
+
+a=input("enter your name")
+b=input("enter your age")
+d={'name':a,
+   'age':b}
+print(d)
+
+#Q6 optional(not done)
+
+
+#Q7
+a=("mississippi")
+print(a.count('s'))
+
+
